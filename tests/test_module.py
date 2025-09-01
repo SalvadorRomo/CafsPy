45680ff188362399d7b879287f535c07f762985f
Miguel De-la-Torre <mikefixer@hotmail.com>
2025-09-01 13:28:01 -0600

    Update test_module.py to be used as a script
    The test_module.py to be used as a script, no need for pip installation but using directly the source code from the src directory.

--- tests/test_module.py
+++ tests/test_module.py
@@ -4,6 +4,14 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression, LinearRegression
 import numpy as np
 
+# Optional - to run the code from source code, without installing cafspy using pip...
+# Add the path of the module to run directly the test file on "tests" location 
+import sys
+sys.path.append("./../src")
+
+# Add data location to all functions, in order to run the test_module directly as a script.
+datapath = "../data/"
+
 from cafspy import ICAFS
 from cafspy import CAFS
 # Opt-in to the new behavior to disable silent downcasting
@@ -176,3 +184,12 @@ def test_cafs_contain_only_numbers():
     with pytest.raises(TypeError):
         lr_algo = KNeighborsClassifier(n_neighbors=3)
         CAFS(covering_array,X_algarrobo,y_algarrobo,10,lr_algo,True)
+
+
+
+# Modify here to run test function that will be used
+print("CAFS-Cacao")
+test_cafs_cacao()
+
+print("iCAFS-Cacao")
+test_icafs_cacao()
